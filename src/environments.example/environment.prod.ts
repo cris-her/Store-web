bb880f9f5ee226d581aa7a042feb7cc18a2c3f85
CrisFiss <crs.hdz.s@gmail.com>
2021-04-25 16:42:14 -0500

    first commit

---
+++ src/environments.example/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  url_api: 'https://platzi-store.herokuapp.com',
+  firebase: {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+  }
+};
---
+++ src/environments.example/environment.stag.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  url_api: 'https://platzi-store-stag.herokuapp.com',
+  firebase: {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+  }
+};
